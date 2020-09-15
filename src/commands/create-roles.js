19f2dc0de4a6b46d9f48dab8f133ce9ebf1a0cb6
Niel Hekkens <hello@nielhekkens.nl>
2020-09-15 21:01:26 +0200

    Create the team roles

--- src/commands/create-roles.js
+++ src/commands/create-roles.js
@@ -1,8 +1,10 @@
 const axios = require('axios');
+// permissions calculator: https://discordapi.com/permissions.html#104188993
+// Team permissions number: 104188993
 
 module.exports = {
     name: 'create-roles',
-    description: 'Create team roels',
+    description: 'Create team roles',
     // The command goes into the execute block
     execute(message) {
         const author = message.author.username;
@@ -11,36 +13,27 @@ module.exports = {
         if(isAuthorAdmin(author)) {
             const url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRpAu_dPlGTg3GNAaf9VyvN_cnC71ZFEEMPR2dDGXx7FBw7oRL4liWkf5kYKmiNnG_Drr2KOTDgmt7j/pub?gid=0&single=true&output=csv";
 
-            axios.get(url).then(res => res.data.toString());
-			createTeamRoles(guild);
+            axios.get(url).then((result) => {
+                const resultString = result.data.toString();
+                const teamRolesArray = resultString.split("\r\n");
+
+                for(let i = 0; i < teamRolesArray.length; i++) {
+                    guild.createRole({
+                        name: teamRolesArray[i],
+                        permissions: 104188993,
+                        color: 'ORANGE',
+                    }) 
+                }
+            })
+                .then(message.channel.send("Team roles created"))
+                .catch(console.error);;
 		}
 		else {
 			message.channel.send("I'm sorry, Dave. I'm afraid I can't do that.");
 		}
-        // permissions calculator: https://discordapi.com/permissions.html#104188993
     }
 };
 
 function isAuthorAdmin(username) {
     return username === "NielHekkens" ? true : false;
-}
-
-function createTeamRoles(guild) {
-    const teamRoles = fetchTeamRoles();
-    // for(i = 0; i++; teamRoles.length) {
-    //     guild.createRole({
-    //         name: i,
-    //         permissions: 104188993,
-    //         color: 'ORANGE',
-    //     })
-    //         .then(role => console.log(`Created new role with name ${role.name}`))
-    //         .catch(console.error);
-    // }
-}
-
-function fetchTeamRoles() {
-    const url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRpAu_dPlGTg3GNAaf9VyvN_cnC71ZFEEMPR2dDGXx7FBw7oRL4liWkf5kYKmiNnG_Drr2KOTDgmt7j/pub?gid=0&single=true&output=csv";
-
-     return axios.get(url).then(res => res.data.toString());
-    //     .then(result => result.split(","));
 }
