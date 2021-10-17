35721cd75e816e200f76a8b93dcc25f076d76768
CreventicEsports <85103914+CreventicEsports@users.noreply.github.com>
2021-10-17 12:50:25 +0200

    TS (#10)
    * Update sheet.js
    
    * Update teamspeak.js
    
    * Update help.js

--- src/commands/help.js
+++ src/commands/help.js
@@ -11,7 +11,7 @@ module.exports = {
 			.setDescription("These are the commands available:")
 			.addField("!ping", "Want to check if the bot is online?")
 			.addField("!help", "This is where you are now. This gives you a list of available commands.")
-			.addField("!teamspeak", "Gives you the IP address of the NEO Endurance TeamSpeak server.")
+			.addField("!teamspeak", "Gives you the IP address of the Racespot TeamSpeak server.")
 			.addField("!report", "With this command you can submit a protest.")
 			.addField("!served", "Notify race control you served the penalty.")
 			.addField("!sheet", "Gives you the URL of the race control spreadsheet.");
