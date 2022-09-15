a9ad530285538a8c500969c2d7c22aa2debcd4b9
Niel Hekkens <nielhekkens@goodhabitz.com>
2022-09-15 14:36:31 +0200

    ✏️ Update some terms

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,21 +1,27 @@
 const discordJS = require("discord.js");
 
 module.exports = {
-	name: 'help',
-	description: 'Send the user a message on how to use the chatbot',
-	// The command goes into the execure block
-	execute(message) {
-		const helpMessage = new discordJS.RichEmbed()
-			.setColor("#E56A02")
-			.setTitle("Available commands")
-			.setDescription("These are the commands available:")
-			.addField("!ping", "Want to check if the bot is online?")
-			.addField("!help", "This is where you are now. This gives you a list of available commands.")
-			.addField("!teamspeak", "Gives you the IP address of the Racespot TeamSpeak server.")
-			.addField("!report", "With this command you can submit a protest.")
-			.addField("!served", "Notify race control you served the penalty.")
-			.addField("!sheet", "Gives you the URL of the race control spreadsheet.");
-			
-		message.channel.send(helpMessage);
-	}
+  name: "help",
+  description: "Send the user a message on how to use the chatbot",
+  // The command goes into the execure block
+  execute(message) {
+    const helpMessage = new discordJS.RichEmbed()
+      .setColor("#E56A02")
+      .setTitle("Available commands")
+      .setDescription("These are the commands available:")
+      .addField("!ping", "Want to check if the bot is online?")
+      .addField(
+        "!help",
+        "This is where you are now. This gives you a list of available commands."
+      )
+      .addField(
+        "!teamspeak",
+        "Gives you the IP address of the Racespot TeamSpeak server."
+      )
+      .addField("!report", "With this command you can submit a protest.")
+      .addField("!served", "Notify race control you served the penalty.")
+      .addField("!sheet", "Gives you the URL of the Digital Notice Board.");
+
+    message.channel.send(helpMessage);
+  },
 };
